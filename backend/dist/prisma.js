eac136d26e08686744df7e457addfea262c9e9a7
Noam Avitan <noamavitan159753@gmail.com>
2024-02-27 15:38:12 +0200

    d

--- backend/dist/prisma.js
+++ backend/dist/prisma.js
@@ -1,6 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.prisma = void 0;
-var client_1 = require("@prisma/client");
-exports.prisma = new client_1.PrismaClient();
-//# sourceMappingURL=prisma.js.map
+const { PrismaClient } = require("@prisma/client");
+exports.prisma = new PrismaClient();
--- backend/dist/trpc/trpc.js
+++ backend/dist/trpc/trpc.js
@@ -1,16 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.publicProcedure = exports.router = void 0;
-var server_1 = require("@trpc/server");
+const { initTRPC } = require('@trpc/server');
 /**
  * Initialization of tRPC backend
  * Should be done only once per backend!
  */
-var t = server_1.initTRPC.create();
+const t = initTRPC.create();
 /**
  * Export reusable router and procedure helpers
  * that can be used throughout the router
  */
 exports.router = t.router;
 exports.publicProcedure = t.procedure;
-//# sourceMappingURL=trpc.js.map
