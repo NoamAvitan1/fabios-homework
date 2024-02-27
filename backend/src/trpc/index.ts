dedc0b13c40fe6cf9bbaf02fca854e00525b0aed
Noam Avitan <noamavitan159753@gmail.com>
2024-02-27 13:14:25 +0200

    .

--- backend/src/trpc/index.ts
+++ backend/src/trpc/index.ts
@@ -1,12 +1,13 @@
 import { publicProcedure, router } from './trpc';
-import {z} from 'zod'
+// import {z} from 'zod'
 import { prisma } from '../prisma';
-export const ZodUserSchema = z.object({
-  email: z.string().email(),
-  name: z.string(),  
-});
 
-export type UserSchemaType = z.infer<typeof ZodUserSchema>;
+// export const ZodUserSchema = z.object({
+//   email: z.string().email(),
+//   name: z.string(),  
+// });
+
+// export type UserSchemaType = z.infer<typeof ZodUserSchema>;
 
 export const appRouter = router({
   user: publicProcedure.query(async (opts) => {
