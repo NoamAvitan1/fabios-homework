b07489cf0b629440f374b25dba2b7980cce29029
Noam Avitan <noamavitan159753@gmail.com>
2024-02-29 10:59:12 +0200

    deleted unnecessary

--- frontend/src/components/user/UsersList.tsx
+++ frontend/src/components/user/UsersList.tsx
@@ -32,19 +32,19 @@ export const UsersList = (props: Props) => {
     onSuccess: () => {
       users_list.refetch();
     },
-  })
+  });
   const updateUser = trpc.updateUser.useMutation({
-    onSuccess: async() => {
+    onSuccess: async () => {
       await users_list.refetch();
-      const user = users_list?.data?.find((obj) => obj.id === userData?.id)
-      if(user){
+      const user = users_list?.data?.find((obj) => obj.id === userData?.id);
+      if (user) {
         selectUser(user?.id);
       }
-    }
-  })
+    },
+  });
 
-  const selectUser = (id: string):void => {
-    if(id === userData?.id){
+  const selectUser = (id: string): void => {
+    if (id === userData?.id) {
       setUserData(null);
       return;
     }
@@ -79,27 +79,61 @@ export const UsersList = (props: Props) => {
           </ul>
         </section>
         <div className={`${userData === null ? "list" : "list-selected"}`}>
-          <article className={`${userData === null ?'info' : 'infoData'}`}>
+          <article className={`${userData === null ? "info" : "infoData"}`}>
             {users_list.data?.map((user) => (
               <section
                 onClick={() => selectUser(user?.id)}
-                className={`${userData?.id !== user?.id ? "usersData" : "usersData-selected"}`}
-                key={user?.id}>
+                className={`${
+                  userData?.id !== user?.id ? "usersData" : "usersData-selected"
+                }`}
+                key={user?.id}
+              >
                 <p>{user?.customer}</p>
-                {userData === null ? <p>{user?.date}</p>:null}
-                {userData === null ? <p>{user?.branch}</p>:null}
-                {userData === null ? <p style={{color:`${user?.status === "מאושר" ? 'green' : 'orange'}`}}>{user?.status}</p>:null}
-                {userData === null ? <p style={{ display: "flex", justifyContent: "center" }}>{<BiShekel />}1500</p>:null}
-                {userData === null ?<p onClick={(e)=>{e.stopPropagation(),deleteUser.mutate(user?.id)}} className="delete-icon">{<MdOutlineDeleteOutline/>}</p> :null}
-                {userData === null ?<IoIosArrowRoundBack style={{ fontSize: "20px" }} /> : userData?.id === user?.id ? <IoMdClose/> : null}
+                {userData === null ? <p className="userDate">{user?.date}</p> : null}
+                {userData === null ? <p>{user?.branch}</p> : null}
+                {userData === null ? (
+                  <p
+                    className={`${
+                      user?.status === "מאושר"
+                        ? "status-agreed"
+                        : "status-progress"
+                    }`}
+                  >
+                    {user?.status}
+                  </p>
+                ) : null}
+                {userData === null ? (
+                  <p style={{ display: "flex", justifyContent: "center" }}>
+                    {<BiShekel />}1500
+                  </p>
+                ) : null}
+                {userData === null ? (
+                  <p
+                    onClick={(e) => {
+                      e.stopPropagation(), deleteUser.mutate(user?.id);
+                    }}
+                    className="delete-icon"
+                  >
+                    {<MdOutlineDeleteOutline />}
+                  </p>
+                ) : null}
+                {userData === null ? (
+                  <IoIosArrowRoundBack style={{ fontSize: "20px" }} />
+                ) : userData?.id === user?.id ? (
+                  <IoMdClose />
+                ) : null}
               </section>
             ))}
           </article>
-          {userData && 
-          <section className="user">
-            <IoMdClose onClick={()=>selectUser(userData?.id)} className="icon-close"/> 
-          <SingleUser user={userData} updateUser={updateUser} />
-          </section>}
+          {userData && (
+            <section className="user">
+              <IoMdClose
+                onClick={() => selectUser(userData?.id)}
+                className="icon-close"
+              />
+              <SingleUser user={userData} updateUser={updateUser} />
+            </section>
+          )}
         </div>
       </article>
     </div>
